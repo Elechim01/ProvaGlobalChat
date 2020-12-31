13652494488b508e9b2ae670f930df62a3909586
Elechim01 <mikymanny015@gmail.com>
2020-12-31 11:26:17 +0100

    Initial Commit

---
+++ ProvaGlobalChat/ProvaGlobalChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProvaGlobalChatApp.swift
+//  ProvaGlobalChat
+//
+//  Created by Michele on 31/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ProvaGlobalChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
